7023377ae7a8d184120d6d7bbb364fcb35b90161
Michael Xavier <michael@michaelxavier.net>
2011-09-24 00:27:16 -0700

    Doc the main module
    After super basic pagination, should be ready for first release

--- Web/GooglePlus.hs
+++ Web/GooglePlus.hs
@@ -1,3 +1,39 @@
+--------------------------------------------------------------------
+-- |
+-- Module      : Web.GooglePlus
+-- Description : Toplevel module for the Google+ API
+-- Copyright   : (c) Michael Xavier 2011
+-- License     : MIT
+--
+-- Maintainer: Michael Xavier <michael@michaelxavier.net>
+-- Stability : provisional
+-- Portability: portable
+--
+-- Toplevel module for the Google+ API operating in the GooglePlusM Monad. 
+-- Currently covers the (very) limited, read-only API that Google has exposed
+-- in v1 of the Google+ API
+-- 
+-- > {-# LANGUAGE OverloadedStrings #-}
+-- > import Web.GooglePlus
+-- > import Web.Campfire.Monad
+-- > import Web.Campfire.Types
+-- > import Control.Monad.Reader
+-- > import Data.Text (unpack)
+-- > 
+-- > doStuff :: GooglePlusM ()
+-- > doStuff = do
+-- >   Right person <- getPerson Me
+-- >   Right feed   <- getActivityFeed Me PublicCollection
+-- >   -- ...
+-- >   return ()
+-- > 
+-- > main :: IO ()
+-- > main = do
+-- >   runReaderT (unGooglePlusM doStuff) env
+-- >   where env  = GooglePlusEnv { gpAuth = APIKey "MYKEY" }
+-- 
+--------------------------------------------------------------------
+
 {-# LANGUAGE OverloadedStrings #-}
 module Web.GooglePlus (getPerson, getActivity, getActivityFeed) where
 
@@ -17,16 +53,22 @@ import           Data.Text.Encoding (encodeUtf8, decodeUtf8)
 import           Network.HTTP.Enumerator
 import           Network.HTTP.Types (Ascii, Query, QueryItem)
 
+-- | Get a person who matches the given identifier
 getPerson :: PersonID -> GooglePlusM (Either Text Person)
 getPerson pid = genericGet pth []
   where pth = personIdPath pid
 
 
+-- | Get an activity who matches the given activity ID
 getActivity :: ID -> GooglePlusM (Either Text Activity)
 getActivity aid = genericGet pth []
   where pth = append "/plus/v1/activities/" $ encodeUtf8 aid
 
---TODO: pagetoken
+--TODO: pagetoken, pagination, limits, enumerator?
+
+-- | Get an activity who matches the given activity ID and collection to use.
+-- Currently uses the default page size (20) and only fetches the first page. I
+-- plan to add pagination fairly soon and possibly an enumerator a bit later
 getActivityFeed :: PersonID -> ActivityCollection -> GooglePlusM (Either Text ActivityFeed)
 getActivityFeed pid coll = genericGet pth []
   where pth  = append pidP actP
@@ -53,7 +95,6 @@ doGet auth pth q = liftIO $ withManager $ \manager -> do
   return (c, b)
   where req = genRequest auth pth q
 
---TODO: handle oauth
 genRequest :: GooglePlusAuth -> Ascii -> Query -> Request m
 genRequest auth pth q = def { host = h,
                               path = pth,
